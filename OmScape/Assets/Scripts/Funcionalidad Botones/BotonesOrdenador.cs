5329cb30467345162775a052592365af0b926b78
deshinwind <juanantoniosanchezrubio8@gmail.com>
2023-12-18 10:51:07 +0100

    V.1.6
    PC terminado

--- OmScape/Assets/Scripts/Funcionalidad Botones/BotonesOrdenador.cs
+++ OmScape/Assets/Scripts/Funcionalidad Botones/BotonesOrdenador.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class BotonesOrdenador : MonoBehaviour
@@ -10,6 +12,13 @@ public class BotonesOrdenador : MonoBehaviour
     public GameObject inicio;
     public GameObject narrativa;
     public GameObject posesiones;
+
+    public GameObject[] nar;
+    public GameObject[] pos;
+
+    private bool notaActiva;
+    private string nota;
+
 
     private AlmacenNotas almacen;
 
@@ -25,13 +34,18 @@ public class BotonesOrdenador : MonoBehaviour
     {
         if (Input.GetButtonDown("Cancel"))
         {
-            if (inicio.activeSelf)
+            if (notaActiva)
             {
-                SceneManager.LoadScene("H3");
+                DesactivarNota();
+                notaActiva = false;
             }
             else if (narrativa.activeSelf || posesiones.activeSelf)
             {
                 Inicio();
+            }
+            else if (inicio.activeSelf)
+            {
+                SceneManager.LoadScene("H3");
             }
         }
     }
@@ -80,8 +94,105 @@ public class BotonesOrdenador : MonoBehaviour
         inicio.SetActive(true);
     }
 
+    
+    public void NotaNar()
+    {
+        notaActiva = true;
+        nota = EventSystem.current.currentSelectedGameObject.name;
+        switch (nota)
+        {
+            case "N0":
+                nar[0].SetActive(true);
+                break;
+            case "N1":
+                nar[1].SetActive(true);
+                break;
+            case "N2":
+                nar[2].SetActive(true);
+                break;
+            case "N3":
+                nar[3].SetActive(true);
+                break;
+            case "N4":
+                nar[4].SetActive(true);
+                break;
+        }
+    }
+    public void NotaPos()
+    {
+        notaActiva = true;
+        nota = EventSystem.current.currentSelectedGameObject.name;
+
+        print(nota);
+
+        switch (nota)
+        {
+            case "P0":
+                pos[0].SetActive(true);
+                break;
+            case "P1":
+                pos[1].SetActive(true);
+                break;
+            case "P2":
+                pos[2].SetActive(true);
+                break;
+            case "P3":
+                pos[3].SetActive(true);
+                break;
+            case "P4":
+                pos[4].SetActive(true);
+                break;
+            case "P5":
+                pos[5].SetActive(true);
+                break;
+        }
+    }
+
+    public void DesactivarNota()
+    {
+        switch (nota)
+        {
+            case "N0":
+                nar[0].SetActive(false);
+                break;
+            case "N1":
+                nar[1].SetActive(false);
+                break;
+            case "N2":
+                nar[2].SetActive(false);
+                break;
+            case "N3":
+                nar[3].SetActive(false);
+                break;
+            case "N4":
+                nar[4].SetActive(false);
+                break;
+            case "P0":
+                pos[0].SetActive(false);
+                break;
+            case "P1":
+                pos[1].SetActive(false);
+                break;
+            case "P2":
+                pos[2].SetActive(false);
+                break;
+            case "P3":
+                pos[3].SetActive(false);
+                break;
+            case "P4":
+                pos[4].SetActive(false);
+                break;
+            case "P5":
+                pos[5].SetActive(false);
+                break;
+        }
+    }
+
     public void Escape()
     {
         SceneManager.LoadScene("H3");
     }
+    
+    
+
 }
